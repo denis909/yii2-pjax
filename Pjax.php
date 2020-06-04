dac31ad393df478856c08bf492c2b7614f0ab930
denis909 <denis909@mail.ru>
2020-06-04 22:42:58 +0300

    n/a

---
+++ Pjax.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace denis909\yii;
+
+class Pjax extends \yii\widgets\Pjax
+{
+
+    public $enablePushState = true;
+
+    public $enableReplaceState = true;
+
+    public $timeout = 0;
+
+}
